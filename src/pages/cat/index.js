13ea73af5fe09996f5f08ac919ba032559828aa4
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-17 14:26:06 -0500

    Made image and cat components

--- src/pages/cat/index.js
+++ src/pages/cat/index.js
@@ -1,4 +1,16 @@
 // src/pages/cat/index.js
+import catData from '/cat-data.json';
+import CatImage from './CatImage';
+
 export default function Cat() {
-    return <h1>Hi, I'm a cat.</h1>;
+    return (
+        <div>
+            <h1>Hi, I'm a cat.</h1>
+            <CatImage catData={catData} />
+        </div>
+    )
+
   }
+
+
+console.log(catData);
