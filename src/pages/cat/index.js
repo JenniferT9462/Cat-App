1ffc4565933961fa9308e90ce18230fa5ee7f6cc
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-17 20:35:30 -0500

    Add components to cat component

--- src/pages/cat/index.js
+++ src/pages/cat/index.js
@@ -1,12 +1,14 @@
 // src/pages/cat/index.js
 import catData from '/cat-data.json';
 import CatImage from './CatImage';
+import CatDetails from './CatDetails';
 
 export default function Cat() {
     return (
         <div>
             <h1>Hi, I'm a cat.</h1>
             <CatImage catData={catData} />
+            <CatDetails catData={catData} />
         </div>
     )
 
