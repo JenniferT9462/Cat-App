451513585e46f9bc71b747aa3640879c6b3b4015
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-17 20:40:52 -0500

    Add the rest of components

--- src/pages/cat/CatImage.js
+++ src/pages/cat/CatImage.js
@@ -1,5 +1,11 @@
 // src/pages/cat/CatImage.js
 export default function CatImage(props) {
     const imgURL = props.catData.image_link;
-    return <img src={imgURL} alt="Cat" />;
+    return (
+      <div>
+        <h4>Hi I'm a {props.catData.name} cat!</h4>
+        <img src={imgURL} alt="Cat" />
+      </div>
+      
+    );
   }
--- src/pages/cat/index.js
+++ src/pages/cat/index.js
@@ -6,7 +6,7 @@ import CatDetails from './CatDetails';
 export default function Cat() {
     return (
         <div>
-            <h1>Hi, I'm a cat.</h1>
+            <h1>Cats Cats Cats</h1>
             <CatImage catData={catData} />
             <CatDetails catData={catData} />
         </div>
