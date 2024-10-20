d5a900ceb4da1359bdbe47e248b93bdb2f126612
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-20 15:30:23 -0500

    Finishing touches

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 // src/pages/index.js
 export default function Home() {
-  return <div>
-    <h1 className="text-4xl text-center">Welcome to the Cat App!</h1>
-    <img src="/splashImage.jpeg"/>
-  </div>;
+  return (
+    <div>
+      <h1 className="m-10 text-4xl text-center">Welcome to the Cat App!</h1>
+      <img src="/splashPage.jpeg"/>
+    </div>
+  );
 }
