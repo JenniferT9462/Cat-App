68996990998853753bb2fd7e3feed493850526da
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-24 18:50:10 -0500

    Fixed link to cat?

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="text-center">
       <h1 className="m-10 text-4xl">Welcome to the Cat App!</h1>
-      <a className="bg-white text-black p-4" href="/cat">Go to Cat App</a>
+      <Link className="bg-white text-black p-4" href="/cat">Go to Cat App</Link>
       <img src="/splashPage.jpeg"/>
       
     </div>
