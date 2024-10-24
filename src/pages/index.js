f136c6e7ceba7682edebea5a2358fdcedca21f1d
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-23 20:59:26 -0500

    Add link to cat app page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 // src/pages/index.js
 export default function Home() {
   return (
-    <div>
-      <h1 className="m-10 text-4xl text-center">Welcome to the Cat App!</h1>
+    <div className="text-center">
+      <h1 className="m-10 text-4xl">Welcome to the Cat App!</h1>
+      <a className="bg-white text-black p-4" href="/cat">Go to Cat App</a>
       <img src="/splashPage.jpeg"/>
+      
     </div>
   );
 }
