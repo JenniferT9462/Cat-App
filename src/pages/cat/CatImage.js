0b2f15546ead882be24d4ccaea6dd4cb1eefb2d8
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-29 18:17:56 -0500

    Fixed errors

--- src/pages/cat/CatImage.js
+++ src/pages/cat/CatImage.js
@@ -3,12 +3,12 @@ export default function CatImage(props) {
     // const imgURL = props.image_link;
     return (
       <div className="text-black m-12">
-        <div className=" flex justify-center">
-          <div >
-            <img className="rounded-full" width="340" src={props.image_link} alt="Cat" />
-            <h4 className="text-center text-xl font-bold">Hi I&apos;m a {props.name}!</h4>
+          <div className=" flex justify-center">
+            <div >
+              <img className="rounded-full" width="340" src={props.image_link} alt="Cat" />
+              <h4 className="text-center text-xl font-bold">Hi I&apos;m a {props.name}!</h4>
+            </div>
           </div>
-        </div>
-
+      </div>
     );
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 // src/pages/index.js
 
-import Link from "next/link";
+
 
 
 export default function Home() {
