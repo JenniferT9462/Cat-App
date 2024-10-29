e66c9b01a968c0277422b127096e310a5eafcad8
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-29 17:30:22 -0500

    Add link to more cats

--- src/pages/cat/Header.js
+++ src/pages/cat/Header.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <div className="drop-shadow-md">
-            <h1 className="text-center text-xl bg-orange-400">Cats Cats and More Cats</h1>
+        <div className="bg-orange-400 drop-shadow-md flex justify-between">
+            <h1 className="text-xl">Cats Cats and More Cats</h1>
+            <Link href="/more-cats">More Cats</Link>
         </div>
     )
 }
