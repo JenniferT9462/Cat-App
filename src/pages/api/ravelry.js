4d6c1c8d213b3943ec38bdddeb3a25f8ed466fb2
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-30 15:59:32 -0500

    Got ravelry api to fetch patterns with basic auth hidden in env

--- src/pages/api/ravelry.js
+++ src/pages/api/ravelry.js
@@ -3,3 +3,23 @@ const password = process.env.RAVELRY_PASSWORD;
 
 const headers = new Headers();
 headers.set('Authorization', 'Basic ' + btoa(username + ':' + password));
+
+async function fetchFromRavelry(queryPattern) {
+    const url = `https://api.ravelry.com/patterns/search.json?query=${queryPattern}+free`;
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: headers
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+export default async function handler(req, res) {
+    const query = req.query;
+    console.log(query);
+    const patterns = await fetchFromRavelry("Halloween Hats");
+    console.log(patterns);
+
+    res.status(200).json(patterns)
+}
