6e0e32e7e44586dcf87579d3ea56b174c59d645f
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-02 15:18:06 -0500

    Add height to image in CatImage.js

--- src/pages/cat/CatImage.js
+++ src/pages/cat/CatImage.js
@@ -7,7 +7,15 @@ export default function CatImage(props) {
       <div className="text-black m-12">
           <div className=" flex justify-center">
             <div >
-              <Image className="rounded-full" width="340" src={props.image_link} alt="Cat" />
+              <Image 
+                className="rounded-full" 
+                width={340}
+                height={340} 
+                src={props.image_link} 
+                alt="Cat" 
+                //priority prop to make image load quickly
+                priority
+                />
               <h4 className="text-center text-xl font-bold">Hi I&apos;m a {props.name}!</h4>
             </div>
           </div>
