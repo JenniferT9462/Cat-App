78e20395e57c1302cdb4463daecb6b5e072d4aad
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-02 13:48:14 -0500

    Import Image and updated every img

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
+import Image from "next/image";
 import Link from "next/link";
-
-// src/pages/index.js
-
-
 
 
 export default function Home() {
@@ -10,7 +7,7 @@ export default function Home() {
     <div className="text-center">
       <h1 className="m-10 text-4xl">Welcome to the Cat App!</h1>
       <Link className="bg-white text-black p-4" href="/cat">Go to Cat App</Link>
-      <img src="/splashPage.jpeg"/>
+      <Image src="/splashPage.jpeg"/>
       
       
     </div>
