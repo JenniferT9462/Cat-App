c8c930d79030261f5ed955c580a0a7edec218ed9
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-02 15:05:11 -0500

    Fixed splash page to work with Image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,25 @@ import Image from "next/image";
 import Link from "next/link";
 
 
+
 export default function Home() {
   return (
     <div className="text-center">
       <h1 className="m-10 text-4xl">Welcome to the Cat App!</h1>
-      <Link className="bg-white text-black p-4" alt="Ai Burmese Cat Image" href="/cat">Go to Cat App</Link>
-      <Image src="/splashPage.jpeg"/>
-      
-      
+      <Link className="bg-white text-black p-4" href="/cat">Go to Cat App</Link>
+      {/* Wrap Image in div w/relative class and w-full h-screen to make the entire viewport height */}
+      <div className="relative w-full h-screen">
+        <Image 
+          src="/splashPage.jpeg" 
+          alt="Ai Burmese Cat" 
+          // layout prop to make the image fill its parent container
+          layout="fill" 
+          // objectFit prop to make sure the image covers the entire area and maintain aspect ratio
+          objectFit="cover" 
+          // priority prop to ensure image will be loaded asap
+          priority
+        />
+      </div>
     </div>
   );
 }
