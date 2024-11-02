357d3f6f32fcc692627b35900f8cfffeb89e7e9c
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-02 15:17:38 -0500

    Add api-ninjas domain to next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['api-ninjas.com'], // Add the hostname here
+  },
 };
 
 export default nextConfig;
+
