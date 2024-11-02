f1c57d49a1a930452266929b40a3356f91aa0488
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-02 15:24:50 -0500

    Add height and width to Image component

--- src/pages/more-cats/CatCard.js
+++ src/pages/more-cats/CatCard.js
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function CatCard(props) {
     return (
         <div className="bg-white rounded-lg shadow-md overflow-hidden max-w-sm mx-auto">
-            <Image className="w-full object-cover" src={props.image} alt={props.name} />
+            <Image 
+                className="w-full object-cover" 
+                src={props.image} 
+                alt={props.name} 
+                width={300}
+                height={300}
+                priority
+                />
             <div className="p-4">
                 <h2 className="text-xl font-bold text-gray-800">{props.name}</h2>
                 <p className="text-gray-600">{props.origin}</p>
