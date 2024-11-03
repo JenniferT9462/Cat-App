f18b4366c2b00e548f6f975c63106d2514a34b1c
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-03 17:12:39 -0600

    Add domian for pokemon image to config file

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['api-ninjas.com'], // Add the hostname here
-  },
+    domains: 
+      ['api-ninjas.com'], 
+    domains:
+      ['raw.githubusercontent.com']// Add the hostname here
+  }
 };
+
+
 
 export default nextConfig;
 
