969812bbb6fe398d5ba91fbe748783b87b94b8dc
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-04 09:30:01 -0600

    Style trivia page with tailwind

--- src/pages/trivia/index.js
+++ src/pages/trivia/index.js
@@ -8,7 +8,14 @@ export default function Trivia() {
     async function fetchTrivia() {
         try {
             const response = await fetch('/api/trivia');
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            //Read the response as text
             const data = await response.json();
+            console.log('API Response:', data);
+            
             setTriviaData(data);
         } catch (error) {
             console.error('Error fetching Trivia Questions:', error);
@@ -23,14 +30,14 @@ export default function Trivia() {
     }
 
     return (
-        <div className='bg-amber-100'>
+        <div className='bg-amber-100 h-screen'>
             <Header/>
-            <div className="text-black">
-                <h1>Animal Trivia!</h1>
+            <div className="text-black p-4 rounded-lg shadow-md bg-white mt-6 w-2/3 m-auto">
+                <h1 className='text-2xl font-bold mb-4'>Animal Trivia!</h1>
                 {triviaData.map((question, index) =>(
                     <div key={index}>
-                        <p>Question: {question.trivia}</p>
-                        <p>Answer: {question.answer}</p>
+                        <p className='text-amber-900 bg-amber-100 p-2 rounded-md border border-amber-300 shadow-md font-semibold'>Question: {question.trivia}</p>
+                        <p className='text-amber-900 bg-amber-50 p-2 rounded-md cursor-pointer hover:bg-amber-100'>Answer: {question.answer}</p>
                     </div>
                 ))}
             </div>
